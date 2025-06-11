bec3901a4f5f487c5c4d90f7e5c445bfaf22286a
wbhaoshuai <1402078577@qq.com>
2025-06-11 16:54:47 +0800

    Update UDPclient: prepare DOWNLOAD message to request the files on the list
    Added code to establish a connection with the server. And the main function is added.

--- client/UDPclient.py
+++ client/UDPclient.py
@@ -1,30 +1,39 @@
 import sys
 import socket
 
-# Validate whether sufficient parameters are provided
-if len(sys.argv) < 4:
-    print("You should enter: Python udpclient.py host_name port_number file_name")
-    sys.exit(1)
+def main():
+    # Validate whether sufficient parameters are provided
+    if len(sys.argv) < 4:
+        print("You should enter: Python udpclient.py host_name port_number file_name")
+        sys.exit(1)
 
-# Process the command line arguments
-host_name = sys.argv[1]
-port_number = sys.argv[2]
-file_name = sys.argv[3]
+    # Process the command line arguments
+    host_name = sys.argv[1]
+    port_number = sys.argv[2]
+    file_name = sys.argv[3]
 
-# Save the name of the file to download
-request_file = []
+    # Save the name of the file to download
+    request_file = []
 
-try:
-    # Open the file for reading
-    with open(file_name, 'r', encoding='utf-8') as file:
-        line = file.readline()
-        while line:
-            line = line.strip()
-            # containing the list of files to download
-            request_file.append(line)
-            line = file.readline()       
-except FileNotFoundError:
-    print("File not found, please check the file path.")   
+    try:
+        # Open the file for reading
+        with open(file_name, 'r', encoding='utf-8') as file:
+            line = file.readline()
+            while line:
+                line = line.strip()
+                # containing the list of files to download
+                request_file.append(line)
+                line = file.readline()       
+    except FileNotFoundError:
+        print("File not found, please check the file path.")   
 
-# Create datagram socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Create a UDP socket object and link it to the server
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.connect((host_name, port_number))
+    for i in range(len(request_file)):
+        # prepare DOWNLOAD message to request the first file on the list
+        DOWNLOAD_reaquest = "DOWNLOAD " + request_file[i]
+        print(DOWNLOAD_reaquest)
+
+if __name__ == "__main__":
+   main()
