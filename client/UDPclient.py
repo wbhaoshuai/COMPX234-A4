919b76f6331988a7dd1d348b4b36bfadef7ededf
wbhaoshuai <1402078577@qq.com>
2025-06-11 16:33:56 +0800

    Update UDPclient.py:  open the file containing the list of files to download
    Get the file name to download

--- client/UDPclient.py
+++ client/UDPclient.py
@@ -9,3 +9,18 @@ if len(sys.argv) < 4:
 host_name = sys.argv[1]
 port_number = sys.argv[2]
 file_name = sys.argv[3]
+
+# Save the name of the file to download
+request_file = []
+
+try:
+    # Open the file for reading
+    with open(file_name, 'r', encoding='utf-8') as file:
+        line = file.readline()
+        while line:
+            line = line.strip()
+            # containing the list of files to download
+            request_file.append(line)
+            line = file.readline()       
+except FileNotFoundError:
+    print("File not found, please check the file path.")   
